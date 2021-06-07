64ca7dc4581ee29bfb3ea5b8af4d428780dae427
moizahmed97 <moizahmed97@gmail.com>
2021-06-07 13:51:04 +0300

    add technicians list page

--- technicians/urls.py
+++ technicians/urls.py
@@ -1,7 +1,9 @@
+from technicians.apps import TechniciansConfig
 from django.urls import path
+from .views import TechinicansListView
 
 app_name = 'technicians'
 
 urlpatterns = [
-
+    path('', TechinicansListView.as_view(), name="technicians-list")
 ]
--- technicians/views.py
+++ technicians/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from issues.models import Technician
 
 
 class TechinicansListView(LoginRequiredMixin, generic.ListView):
-    template_name = "technicians/technician_list.html"
+    template_name = "technicians/technicians_list.html"
+
+    def get_queryset(self):
+        return Technician.objects.all()
