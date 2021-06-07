e2f38767d0cc0ef64a5c42bf83f9b994a839d71a
moizahmed97 <moizahmed97@gmail.com>
2021-06-07 13:50:31 +0300

    add technician model

--- issues/admin.py
+++ issues/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from issues.models import User,Issue,Issuer
+from issues.models import User,Issue,Issuer, Technician
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Issue)
 admin.site.register(Issuer)
+admin.site.register(Technician)
