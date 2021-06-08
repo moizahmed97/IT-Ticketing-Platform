be0760158b4edbfe5d0fca7ba44f20af1f433871
moizahmed97 <moizahmed97@gmail.com>
2021-06-08 14:44:49 +0300

    add delete tech functionality

--- technicians/urls.py
+++ technicians/urls.py
@@ -1,9 +1,12 @@
 from technicians.apps import TechniciansConfig
 from django.urls import path
-from .views import TechinicansListView
+from .views import TechinicansListView, TechnicianCreateView,TechnicianDetailView, TechnicianDeleteView
 
 app_name = 'technicians'
 
 urlpatterns = [
-    path('', TechinicansListView.as_view(), name="technicians-list")
-]
+    path('', TechinicansListView.as_view(), name="technician-list"),
+    path('create/', TechnicianCreateView.as_view(), name="technician-create"),
+    path('<int:pk>/', TechnicianDetailView.as_view(), name="technician-detail"),
+    path('<int:pk>/delete', TechnicianDeleteView.as_view(), name="technician-delete")
+]   
--- technicians/views.py
+++ technicians/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from issues.models import Technician
+from .forms import TechnicianModelForm
 
 
 class TechinicansListView(LoginRequiredMixin, generic.ListView):
@@ -9,3 +10,29 @@ class TechinicansListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Technician.objects.all()
+
+class TechnicianCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name="technicians/technician_form.html"
+    form_class = TechnicianModelForm
+
+    def form_valid(self, form):
+        user = form.save(commit=False) 
+        user.is_technician = True
+        user.set_password("!Qaz2wsx")
+        user.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('technicians:technician-list')    
+
+
+class TechnicianDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Technician 
+    template_name = "technicians/technician_detail.html"        
+    context_object_name = "technician"
+
+class TechnicianDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "technicians/technician_delete_confirm.html"
+    model = Technician
+    def get_success_url(self):
+        return reverse('technicians:technician-list')
